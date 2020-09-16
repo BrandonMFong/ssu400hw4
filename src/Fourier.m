436491e5b14913f65dd57719c268c23ea52890a7
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-16 15:26:28 -0700

    writing params

--- src/Fourier.m
+++ src/Fourier.m
@@ -3,3 +3,10 @@ fprintf('Starting program...');
 
 % Let's work on rect function first 
 % Let's try to plot the function 
+fprintf("Plotting f(t) = V*rect((t-to)/T)");
+T = input("Enter value for T ")
+to = input("Enter value for to ")
+t = input("Enter value for t ")
+V = input("Enter value for V ")
+
+fprintf("f(t) = %f*rect((%f-%f)/%f)",V, t, to, T);
